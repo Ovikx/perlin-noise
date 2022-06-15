4d69df4b709f69c37c5e5b928de2d8a7d54db4f6
Ovik <47368700+Ovikx@users.noreply.github.com>
2022-06-15 19:01:20 -0400

    deleted js files

--- noiseTester.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const NoiseGenerator_1 = require("./src/NoiseGenerator");
-let gen = new NoiseGenerator_1.NoiseGenerator();
-console.log(gen.generate(4, 3));
-//console.log(gen.normalizeMatrix([[1,2],[3,4],[-1,5]]));
